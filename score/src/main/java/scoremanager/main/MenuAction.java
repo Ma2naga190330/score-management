ba221a402e3128a1d642eb0fb5c87ac912733dd7
松永悠志 <myz0330fuklow0330@gmail.com>
2026-04-14 13:17:20 +0900

    松永メニューから一覧に遷移するURLの変更

--- score/src/main/java/scoremanager/main/MenuAction.java
+++ score/src/main/java/scoremanager/main/MenuAction.java
@@ -10,6 +10,6 @@ import tool.Action;
 public class MenuAction extends Action{
 	@Override
 	public void execute(HttpServletRequest request,HttpServletResponse response)throws ServletException,IOException {
-		request.getRequestDispatcher("menu.jsp").forward(request, response);
+		request.getRequestDispatcher("../scoremanager/main/menu.jsp").forward(request, response);
 	}
 }
