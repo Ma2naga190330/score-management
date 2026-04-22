48a2b4a3b536cd89c8b66f6e2a3ecc9809651d8b
e_asano <e_asano@eitaaaaaaaaaaaaaaaaaaaa>
2026-04-22 11:02:20 +0900

    ignore

--- score/src/main/java/scoremanager/LoginExecuteAction.java
+++ score/src/main/java/scoremanager/LoginExecuteAction.java
@@ -10,35 +10,29 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import tool.Action;
 
-public class LoginExecuteAction extends Action {
-    @Override
-    public void execute(HttpServletRequest req, HttpServletResponse res)
-            throws ServletException, IOException {
-        try {
-            String id = req.getParameter("id");
-            String password = req.getParameter("password");
-
-            Teacher t = new Teacher();
-            t.setId(id);
-            t.setPassword(password);
-
-            TeacherDao tDao = new TeacherDao();
-            Teacher user = tDao.loginUser(t);
-
-            if (user != null) {
-                HttpSession session = req.getSession();
-                session.setAttribute("user", user);
-                session.setAttribute("school", user.getSchool());
-                req.getRequestDispatcher("main/Menu.action").forward(req, res);
-                return;
-            } else {
-                req.setAttribute("error", "ログインに失敗しました。IDまたはパスワードが正しくありません");
-                req.getRequestDispatcher("Login.action").forward(req, res);
-                return;
-            }
-
-        } catch (Exception e) {
-            throw new ServletException(e);
-        }
-    }
+public class LoginExecuteAction extends Action{
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse res)throws ServletException,IOException{
+		try {
+			String id = req.getParameter("id");
+			String password = req.getParameter("password");
+			Teacher t = new Teacher();
+			t.setId(id);
+			t.setPassword(password);
+			TeacherDao tDao = new TeacherDao();
+			Teacher user = tDao.loginUser(t);
+			if (user != null) {
+				HttpSession session = req.getSession();
+				session.setAttribute("user", user);
+				req.getRequestDispatcher("main/Menu.action").forward(req, res);
+			}else {
+				String error = "IDまたはPasswordが間違っています";
+				req.setAttribute("error", error);
+				req.getRequestDispatcher("Login.action").forward(req, res);
+			}
+			req.getRequestDispatcher("/error.jsp").forward(req, res);
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
