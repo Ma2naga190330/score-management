bf46723556e0e463a10033c2ee5ebc9314253755
松永悠志 <myz0330fuklow0330@gmail.com>
2026-04-28 13:28:28 +0900

    a

--- score/src/main/java/dao/Dao.java
+++ score/src/main/java/dao/Dao.java
@@ -7,10 +7,12 @@ import javax.sql.DataSource;
 
 public class Dao {
 	static DataSource ds;
+	String url = "jdbc:h2:/home/ubuntu/h2data/point";
+	
 	public Connection getConnection() throws Exception{
 		if (ds==null) {
 			InitialContext ic=new InitialContext();
-			ds=(DataSource)ic.lookup("java:/comp/env/jdbc/point");
+			ds=(DataSource)ic.lookup(url);
 		}
 		return ds.getConnection();
 	}
