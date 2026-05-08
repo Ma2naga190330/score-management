19f61639bc7dd8d06555b22ce2c1a3ecd1668937
a_matsuda <a_matsuda@LAPTOP-86FQ4D76>
2026-05-08 13:52:16 +0900

    i

--- score/src/main/java/scoremanager/LoginAction.java
+++ score/src/main/java/scoremanager/LoginAction.java
@@ -11,7 +11,7 @@ public class LoginAction extends Action {
 	@Override
 	public void execute(HttpServletRequest req,HttpServletResponse res)throws ServletException,IOException{
 		try {
-			String id = "admin";
+			String id = req.getParameter("id");
 			req.setAttribute("id", id);
 			req.getRequestDispatcher("login.jsp").forward(req, res);
 		}catch (Exception e) {
